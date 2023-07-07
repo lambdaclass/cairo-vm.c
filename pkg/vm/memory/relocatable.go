737a42a0925fa494c65debcf6bd62a73a0cf6504
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-07-07 15:59:20 -0300

    Remove unused commented code

--- pkg/vm/memory/relocatable.go
+++ pkg/vm/memory/relocatable.go
@@ -17,11 +17,6 @@ type Int struct {
 	felt uint
 }
 
-// type MaybeRelocatable interface {
-// 	// Later on we will define common methods Relocatable
-// 	// and Int must satisfy
-// }
-
 // MaybeRelocatable is the type of memory cells in the Cairo
 // VM. For now, inner will hold any type but it should be
 // instantiated only with Relocatable or Int types.
