08735174fb151bc850aee312c6749fa51ce6496e
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-07-07 18:25:06 -0300

    Star writing memory tests

---
+++ pkg/vm/memory/memory_test.go
@@ -0,0 +1,27 @@
+package memory_test
+
+import (
+	"testing"
+
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
+
+func TestMemoryInsert(t *testing.T) {
+	data := make([][]memory.MaybeRelocatable, 3, 3)
+	mem := memory.NewMemory(data)
+
+	key := memory.NewRelocatable(0, 0)
+	val := memory.NewMaybeRelocatableInt(5)
+
+	err := mem.Insert(key, val)
+
+	if err != nil {
+		t.Errorf("Insert error in test: %s", err)
+	}
+
+	res_val, err := mem.Get(key)
+
+	if *res_val != val {
+		t.Errorf("Inserted value and original value are not the same")
+	}
+}
