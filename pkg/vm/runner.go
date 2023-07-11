4c4d0f5a9ab8348cf19298f71445e9158c67ca12
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-07-11 16:06:21 -0300

    Save work in progress

--- pkg/vm/runner.go
+++ pkg/vm/runner.go
@@ -1,19 +1,50 @@
 package vm
 
-import "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+import (
+	"errors"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
 
 type CairoRunner struct {
 	program        Program
 	program_base   memory.Relocatable
 	execution_base memory.Relocatable
-	initial_ap     memory.Relocatable
-	initial_fp     memory.Relocatable
+	initial_ap     uint
+	initial_fp     uint
 	initial_pc     memory.Relocatable
 	final_pc       memory.Relocatable
+	entrypoint     uint
 }
 
 // Creates a new instance of a CairoRunner.
 func NewCairoRunner(program Program) *CairoRunner {
 	runner := CairoRunner{program: program}
 	return &runner
+}
+
+func (cr *CairoRunner) InitializeMainEntrypoint(vm *VirtualMachine) (*memory.Relocatable, error) {
+
+	if &cr.entrypoint == nil {
+		return nil, errors.New("Missing main() entrypoint")
+	}
+
+	return nil, nil
+}
+
+func (cr *CairoRunner) InitializeVM(vm *VirtualMachine) error {
+	// FIXME: This `nil` error handling does not sound too good, but leaving
+	// it like this for now
+	var relocatableNil memory.Relocatable
+	var uintNil uint
+	if cr.initial_pc == relocatableNil || cr.initial_ap == uintNil || cr.initial_fp == uintNil {
+		return errors.New("VM initialization error - Could not set initial state")
+	}
+	vm.run_context.pc = cr.initial_pc
+	vm.run_context.ap = cr.initial_ap
+	vm.run_context.fp = cr.initial_fp
+
+	// TODO: Add builtins validation rules and validate memory
+	// See `initialize_vm` method in cairo-vm.
+
+	return nil
 }
