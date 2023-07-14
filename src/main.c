d19a4855dece63c9bf215eaf590b6f38a1b1e9a7
antonio.calvin <garcia.calvin.toni@gmail.com>
2023-07-14 18:13:05 +0200

    WIP: Creating Program structure and parsing data

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
 	//printf("Hello World!\n");
-	FILE *fptr;
-	fptr = fopen("cairo_programs/fibonacci.json", "r");
-	char jsonBuffer[32*1024];
-	size_t nBytes = fread(jsonBuffer, 1, 32 * 1024, fptr);
-	jsonBuffer[nBytes] = 0;
-	parse(jsonBuffer);
+	FILE *fptr = fopen("cairo_programs/fibonacci.json", "r");
+	char buf[32*1024];
+	size_t nBytes = fread(buf, 1, 32 * 1024, fptr);
+	buf[nBytes] = 0;
+	parseJson(buf);
 	return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -8,21 +8,61 @@
 extern "C" {
 #endif
 
-  typedef struct { uint64_t values[4]; } Felt;
-
-  typedef Felt* data[]; 
+  typedef struct {
+    char* filename;
+  } InputFile;
 
   typedef struct {
-    Felt *data;
-    size_t len;
-  } Instructions;
-
+    int end_col;
+    int end_line;
+    InputFile input_file;
+    int start_col;
+    int start_line;
+  } Inst;
 
   typedef struct {
-    Instructions instructions;
+    char** accessible_scopes;
+    int flow_tracking_data_group;
+    int flow_tracking_data_offset;
+    char** hints;
+    Inst inst;
+
+  } InstructionLocation;
+
+  typedef struct {
+    char* cairo_type;
+    int offset;
+  } Members;
+
+  typedef struct {
+    int pc;
+    char* type;
+    Members* members;
+    int size;
+  } Identifiers;
+
+  typedef struct {
+    char* start;
+  } FileContent;
+
+  typedef struct {
+    FileContent fileContent;
+    InstructionLocation* instruction_location;
+  } DebugInfo;
+
+  typedef struct {
+    char **attributes;
+    char **builtins;
+    char *compiler_version;
+    char **data;
+    DebugInfo debug_info;
+    char** hints;
+    Identifiers* identifiers;
+    char* main_scope;
+    char* prime;
   } Program;
 
-  Program parse(const char *jsonDataStr);
+  Program parseJson(const char* dataBuffer);
 
 #ifdef __cplusplus
 } // extern "C"
