f5cb5c32d06cff9e96471ac33a23969c36e060dd
Milton <milton.scuderi@lambdaclass.com>
2023-07-15 13:58:20 -0300

    Decoding Structs

---
+++ src/decode.h
@@ -0,0 +1,91 @@
+#ifndef DECODE_H
+#define DECODE_H
+
+//  Structure of the 63-bit that form the first word of each instruction.
+//  See Cairo whitepaper, page 32 - https://eprint.iacr.org/2021/1063.pdf.
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                     off_dst (biased representation)                     │
+// ├─────────────────────────────────────────────────────────────────────────┤
+// │                     off_op0 (biased representation)                     │
+// ├─────────────────────────────────────────────────────────────────────────┤
+// │                     off_op1 (biased representation)                     │
+// ├─────┬─────┬───────┬───────┬───────────┬────────┬───────────────────┬────┤
+// │ dst │ op0 │  op1  │  res  │    pc     │   ap   │      opcode       │ 0  │
+// │ reg │ reg │  src  │ logic │  update   │ update │                   │    │
+// ├─────┼─────┼───┬───┼───┬───┼───┬───┬───┼───┬────┼────┬────┬────┬────┼────┤
+// │  0  │  1  │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 10 │ 11 │ 12 │ 13 │ 14 │ 15 │
+// └─────┴─────┴───┴───┴───┴───┴───┴───┴───┴───┴────┴────┴────┴────┴────┴────┘
+
+// Instruction is the representation of the first word of each Cairo instruction.
+// Some instructions spread over two words when they use an immediate value, so
+// representing the first one with this struct is enougth.
+
+enum Register
+{
+	REG_AP,
+	REG_FP
+};
+
+enum Op1Addr
+{
+	Imm,
+	OP_AP,
+	OP_FP,
+	Op0
+};
+
+enum Res
+{
+	Op1,
+	Res_Add,
+	Mul,
+	Unconstrained,
+};
+
+enum PcUpdate
+{
+	PC_Regular,
+	Jump,
+	JumpRel,
+	Jnz,
+};
+
+enum ApUpdate
+{
+	AP_Regular,
+	Add,
+	Add1,
+	Add2,
+};
+
+enum FpUpdate
+{
+	Regular,
+	APPlus2,
+	Dst,
+};
+
+enum Opcode
+{
+	NOp,
+	AssertEq,
+	Call,
+	Ret,
+};
+
+struct Instruction
+{
+	int off0;
+	int off1;
+	int off2;
+	enum Register dst_reint;
+	enum Register op0_register;
+	enum Op1Addr op1_addr;
+	enum Res res;
+	enum PcUpdate pc_update;
+	enum ApUpdate ap_update;
+	enum FpUpdate fp_update;
+	enum Opcode opcode;
+};
+
+#endif
