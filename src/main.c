5c334ee5732a1d5ea574b46957ff9f3a437861c4
antonio.calvin <garcia.calvin.toni@gmail.com>
2023-07-18 14:36:31 +0200

    WIP: Returning main value

--- src/main.c
+++ src/main.c
@@ -4,13 +4,6 @@
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
 {
-    Program *program = parseFibJson();
-    for (int i = 0; i < program->data.num_elements; ++i) {
-        printf("Data %i: %s\n", i + 1, program->data.data[i]);
-        free(program->data.data[i]); // Free each individual data element
-    }
-    free(program->data.data); // Free the data array
-    free(program); // Free the main Program struct
-
+    printf("Hello World!");
     return 0;
 }
