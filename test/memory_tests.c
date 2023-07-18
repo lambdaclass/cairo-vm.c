6982d8b7ffc10a82705b9471840fd89bb22f6506
Federica <fedemoletta@hotmail.com>
2023-07-18 09:49:51 -0300

    Add memory tests

---
+++ test/memory_tests.c
@@ -0,0 +1,41 @@
+#include "memory_tests.h"
+#include <stdio.h>
+
+void load_data_one_element(void) {
+    // Initialize memory
+    memory mem = memory_new();
+    memory_add_segment(&mem);
+    //Initialize data to load
+    struct CList *data = CList_init(sizeof(maybe_relocatable));
+    union maybe_relocatable elem = {.felt =  1};
+    data->add(data, &elem);
+    relocatable ptr = {0,0};
+    // Load data
+    memory_load_data(&mem, ptr, data);
+    // Check memory
+    assert(mem.data->count(mem.data) == 1);
+    maybe_relocatable * first_elem = mem.data->at(mem.data, 0);
+    assert( first_elem->felt == 1);
+}
+
+void load_data_empty(void) {
+    // Initialize memory
+    memory mem = memory_new();
+    memory_add_segment(&mem);
+    //Initialize data to load
+    struct CList *data = CList_init(sizeof(maybe_relocatable));
+    relocatable ptr = {0,0};
+    // Load data
+    memory_load_data(&mem, ptr, data);
+    // Check memory
+    assert(mem.data->count(mem.data) == 1);
+    maybe_relocatable * first_elem = mem.data->at(mem.data, 0);
+    assert( first_elem->felt == 1);
+}
+
+void memory_tests(void)
+{
+	printf("--------------------------------- \n");
+	printf("Test: load_data_one_element \n");
+	load_data_one_element();
+}
--- test/test.c
+++ test/test.c
@@ -1,10 +1,13 @@
+#include "memory_tests.h"
 #include "runner_tests.h"
+#include "memory.h"
 #include <assert.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	runner_tests();
+	//runner_tests();
+	memory_tests();
 	printf("Tests passed!\n");
 
 	return 0;
