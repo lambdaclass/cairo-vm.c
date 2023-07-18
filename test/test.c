6d4496504cdcdb23c7878fd711ccae1c01476969
antonio.calvin <garcia.calvin.toni@gmail.com>
2023-07-18 18:06:16 +0200

    Fixing imports

--- test/test.c
+++ test/test.c
@@ -1,8 +1,7 @@
 #include "decoder_test.h"
 #include "felt_test.h"
 #include "parser_test.h"
-#include <assert.h>
-#include <stdio.h>
+
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv) {
 
