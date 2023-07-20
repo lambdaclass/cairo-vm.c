c2ee7ae73a25b1523e042ece313575eae1fa87a9
Milton <milton.scuderi@lambdaclass.com>
2023-07-20 11:38:17 -0300

    Fix converion functions

--- src/utils.c
+++ src/utils.c
@@ -20,38 +20,15 @@ uint8_t *u64_to_be_bytes(uint64_t value, uint8_t *buffer_bytes) {
 uint64_t u64_from_le_bytes(uint8_t *bytes) {
 	uint64_t value = 0;
 	for (int i = 7; i >= 0; i--) {
-		value |= bytes[i];
-		value = value << (8 * i);
+		value |= bytes[i] << (8 * i);
 	}
 	return value;
 }
 
-// TODO: needs fix
 uint64_t u64_from_be_bytes(uint8_t *bytes) {
 	uint64_t value = 0;
 	for (int i = 0; i < 8; i++) {
-		value |= bytes[i];
-		value = value << (8 * i);
-	}
-	return value;
-}
-
-// ------------
-//   u16 bits
-// ------------
-
-uint8_t *u16_to_le_bytes(uint16_t value, uint8_t *buffer_bytes) {
-	for (int i = 0; i < 2; i++) {
-		buffer_bytes[i] = value >> (8 * i) & 0xFF;
-	}
-	return buffer_bytes;
-}
-
-uint16_t u16_from_le_bytes(uint8_t *bytes) {
-	uint16_t value = 0;
-	for (int i = 1; i >= 0; i--) {
-		value |= bytes[i];
-		value = value << (8 * i);
+		value |= bytes[i] << (8 * i);
 	}
 	return value;
 }
@@ -67,11 +44,59 @@ uint8_t *u32_to_le_bytes(uint32_t value, uint8_t *buffer_bytes) {
 	return buffer_bytes;
 }
 
+uint8_t *u32_to_be_bytes(uint32_t value, uint8_t *buffer_bytes) {
+	for (int i = 3; i >= 0; i--) {
+		buffer_bytes[i] = value >> (8 * i) & 0xFF;
+	}
+	return buffer_bytes;
+}
+
 uint32_t u32_from_le_bytes(uint8_t *bytes) {
 	uint16_t value = 0;
 	for (int i = 3; i >= 0; i--) {
-		value |= bytes[i];
-		value = value << (8 * i);
+		value |= bytes[i] << (8 * i);
+	}
+	return value;
+}
+
+uint32_t u32_from_be_bytes(uint8_t *bytes) {
+	uint64_t value = 0;
+	for (int i = 0; i < 4; i++) {
+		value |= bytes[i] << (8 * i);
+	}
+	return value;
+}
+
+// ------------
+//   u16 bits
+// ------------
+
+uint8_t *u16_to_le_bytes(uint16_t value, uint8_t *buffer_bytes) {
+	for (int i = 0; i < 2; i++) {
+		buffer_bytes[i] = value >> (8 * i) & 0xFF;
+	}
+	return buffer_bytes;
+}
+
+uint8_t *u16_to_be_bytes(uint16_t value, uint8_t *buffer_bytes) {
+	for (int i = 1; i >= 0; i--) {
+		buffer_bytes[i] = value >> (8 * i) & 0xFF;
+	}
+	return buffer_bytes;
+}
+
+uint16_t u16_from_le_bytes(uint8_t *bytes) {
+	uint16_t value = 0;
+	for (int i = 1; i >= 0; i--) {
+		value |= bytes[i] << (8 * i);
+	}
+	return value;
+}
+
+uint16_t u16_from_be_bytes(uint8_t *bytes) {
+	uint64_t value = 0;
+	for (int i = 0; i < 2; i++) {
+		value |= bytes[i] << (8 * i);
 	}
 	return value;
 }
