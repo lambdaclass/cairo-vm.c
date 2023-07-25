90f7961eb5a7ad99ddbb9f72bb145524ef4dcd13
toni-calvin <garcia.calvin.toni@gmail.com>
2023-07-25 17:06:12 +0200

    Clang format

--- src/relocatable.h
+++ src/relocatable.h
@@ -3,23 +3,23 @@
 #include "../lambdaworks/lib/lambdaworks.h"
 #include <stdbool.h>
 
-typedef struct relocatable {
-	unsigned int segment_index;
-	unsigned int offset;
-} relocatable;
+	typedef struct relocatable {
+		unsigned int segment_index;
+		unsigned int offset;
+	} relocatable;
 
-union maybe_relocatable_value {
-	struct relocatable relocatable;
-	felt_t felt;
-};
+	union maybe_relocatable_value {
+		struct relocatable relocatable;
+		felt_t felt;
+	};
 
-typedef struct maybe_relocatable {
-	union maybe_relocatable_value value;
-	bool is_felt;
-} maybe_relocatable;
+	typedef struct maybe_relocatable {
+		union maybe_relocatable_value value;
+		bool is_felt;
+	} maybe_relocatable;
 
-bool maybe_relocatable_equal(maybe_relocatable a, maybe_relocatable b);
+	bool maybe_relocatable_equal(maybe_relocatable a, maybe_relocatable b);
 
-maybe_relocatable maybe_relocatable_from_felt_limbs(limb_t *felt);
+	maybe_relocatable maybe_relocatable_from_felt_limbs(limb_t *felt);
 
 #endif
