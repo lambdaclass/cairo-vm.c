0b4ccc32285cb9d6dd1f44bad6fda1c0845b84c3
Federica <fedemoletta@hotmail.com>
2023-07-26 15:52:18 -0300

    Remove unused fn

--- src/relocatable.c
+++ src/relocatable.c
@@ -16,13 +16,3 @@ maybe_relocatable maybe_relocatable_from_felt_limbs(limb_t *felt) {
 	maybe_relocatable elem = {.is_felt = true, .value = {.felt = {felt[0], felt[1], felt[2], felt[3]}}};
 	return elem;
 }
-
-void print_maybe_relocatable(maybe_relocatable *v) {
-	if (v->is_felt) {
-		limb_t *f = v->value.felt;
-		printf("MAYBE_RELOCATABLE:FELT [%llu, %llu, %llu, %llu]\n", f[0], f[1], f[2], f[3]);
-	} else {
-		relocatable r = v->value.relocatable;
-		printf("MAYBE_RELOCATABLE:RELOCATABLE %i:%i\n", r.segment_index, r.offset);
-	}
-}
